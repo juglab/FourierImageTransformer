3c8b8b3cc7efc98d53960ed6953970fb8dbc7740
tibuch <tibuch@mpi-cbg.de>
2021-03-01 22:23:35 +0100

    * Add fast prediction super-res transformer. * Add val-image log.

--- fit/modules/SResTransformerModule.py
+++ fit/modules/SResTransformerModule.py
@@ -2,7 +2,7 @@ import torch
 from pytorch_lightning import LightningModule
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-from fit.transformers.SResTransformer import SResTransformer
+from fit.transformers.SResTransformer import SResTransformerTrain, SResTransformerPredict
 from fit.utils import convert2FC, PSNR, convert_to_dft
 from fit.utils.RAdam import RAdam
 
@@ -14,7 +14,8 @@ from fit.utils.utils import denormalize, denormalize_amp, denormalize_phi
 
 
 class SResTransformerModule(LightningModule):
-    def __init__(self, d_model, img_shape=27,
+    def __init__(self, d_model, img_shape,
+                 x_coords_img, y_coords_img, dst_flatten_order, dst_order,
                  lr=0.0001,
                  weight_decay=0.01,
                  n_layers=4, n_heads=4, d_query=4, dropout=0.1, attention_dropout=0.1):
@@ -30,35 +31,29 @@ class SResTransformerModule(LightningModule):
                                   "dropout",
                                   "attention_dropout")
 
-        self.x_coords_img, self.y_coords_img, self.dst_flatten_coords, self.dst_order = self._get_fc_coords(
-            img_shape=img_shape)
+        self.x_coords_img = x_coords_img
+        self.y_coords_img = y_coords_img
+        self.dst_flatten_order = dst_flatten_order
+        self.dst_order = dst_order
         self.dft_shape = (img_shape, img_shape // 2 + 1)
 
-        self.sres = SResTransformer(d_model=self.hparams.d_model,
-                                    y_coords_img=self.y_coords_img, x_coords_img=self.x_coords_img, flatten_order=self.dst_flatten_coords,
-                                    attention_type='causal-linear',
-                                    n_layers=self.hparams.n_layers,
-                                    n_heads=self.hparams.n_heads,
-                                    d_query=self.hparams.d_query,
-                                    dropout=self.hparams.dropout,
-                                    attention_dropout=self.hparams.attention_dropout)
+        self.sres = SResTransformerTrain(d_model=self.hparams.d_model,
+                                         y_coords_img=self.y_coords_img, x_coords_img=self.x_coords_img,
+                                         flatten_order=self.dst_flatten_order,
+                                         attention_type='causal-linear',
+                                         n_layers=self.hparams.n_layers,
+                                         n_heads=self.hparams.n_heads,
+                                         d_query=self.hparams.d_query,
+                                         dropout=self.hparams.dropout,
+                                         attention_dropout=self.hparams.attention_dropout)
+        self.sres_pred = None
+        x, y = np.meshgrid(range(self.dft_shape[1]), range(-self.dft_shape[0] // 2, self.dft_shape[0] // 2 + 1))
+        radii = np.roll(np.sqrt(x ** 2 + y ** 2, dtype=np.float32), self.dft_shape[0] // 2 + 1, 0)
+        num_shells = 4
+        self.input_seq_length = np.sum(np.round(radii) < num_shells)
 
     def forward(self, x):
         return self.sres.forward(x)
-
-    def _get_fc_coords(self, img_shape):
-        xcoords, ycoords = np.meshgrid(np.linspace(0, img_shape // 2, num=img_shape // 2 + 1, endpoint=True),
-                                       np.concatenate([np.linspace(0, img_shape // 2, img_shape // 2, False),
-                                                       np.linspace(img_shape // 2, img_shape - 1, img_shape // 2 + 1)]))
-
-        order = np.sqrt(xcoords ** 2 + (ycoords - (img_shape // 2)) ** 2)
-        order = np.roll(order, img_shape // 2 + 1, 0)
-        xcoords = np.roll(xcoords, img_shape // 2 + 1, 0)
-        ycoords = np.roll(ycoords, img_shape // 2 + 1, 0)
-        flatten_indices = np.argsort(order.flatten())
-        xcoords = xcoords.flatten()[flatten_indices]
-        ycoords = ycoords.flatten()[flatten_indices]
-        return torch.from_numpy(xcoords), torch.from_numpy(ycoords), torch.from_numpy(flatten_indices), order
 
     def configure_optimizers(self):
         optimizer = RAdam(self.sres.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay)
@@ -76,9 +71,9 @@ class SResTransformerModule(LightningModule):
         pred_phi = denormalize_phi(pred_fc[..., 1])
         target_phi = denormalize_phi(target_fc[..., 1])
 
-        amp_loss = torch.pow(pred_amp - target_amp, 2)
-        phi_loss = 1 - torch.cos(pred_phi - target_phi)
-        return torch.mean(amp_loss + phi_loss), torch.mean(amp_loss), torch.mean(phi_loss)
+        amp_loss = 1 + torch.pow(pred_amp - target_amp, 2)
+        phi_loss = 2 - torch.cos(pred_phi - target_phi)
+        return torch.mean(amp_loss * phi_loss), torch.mean(amp_loss), torch.mean(phi_loss)
 
     def criterion(self, pred_fc, target_fc, mag_min, mag_max):
         fc_loss, amp_loss, phi_loss = self._fc_loss(pred_fc=pred_fc, target_fc=target_fc, mag_min=mag_min,
@@ -87,8 +82,8 @@ class SResTransformerModule(LightningModule):
 
     def training_step(self, batch, batch_idx):
         fc, (mag_min, mag_max) = batch
-        x_fc = fc[:, self.dst_flatten_coords][:, :-1]
-        y_fc = fc[:, self.dst_flatten_coords][:, 1:]
+        x_fc = fc[:, self.dst_flatten_order][:, :-1]
+        y_fc = fc[:, self.dst_flatten_order][:, 1:]
 
         pred = self.sres.forward(x_fc)
 
@@ -105,37 +100,31 @@ class SResTransformerModule(LightningModule):
 
     def validation_step(self, batch, batch_idx):
         fc, (mag_min, mag_max) = batch
-        x_fc = fc[:, self.dst_flatten_coords][:, :-1]
-        y_fc = fc[:, self.dst_flatten_coords][:, 1:]
+        x_fc = fc[:, self.dst_flatten_order][:, :-1]
+        y_fc = fc[:, self.dst_flatten_order][:, 1:]
 
         pred = self.sres.forward(x_fc)
 
         val_loss, amp_loss, phi_loss = self.criterion(pred, y_fc, mag_min, mag_max)
-
-        self.log_dict({'val_loss': val_loss})
         if batch_idx == 0:
-            self.log_val_images(pred, x_fc, y_fc, mag_min, mag_max)
+            self.log_val_images(fc, mag_min, mag_max)
+        self.log_dict({'val_loss': val_loss})
         return {'val_loss': val_loss, 'amp_loss': amp_loss, 'phi_loss': phi_loss}
 
-    def log_val_images(self, pred, x, y_fc, mag_min, mag_max):
-        pred = torch.cat([x[:, :1], pred], dim=1)
-        y_fc = torch.cat([x[:, :1], y_fc], dim=1)
+    def log_val_images(self, fc, mag_min, mag_max):
+        self.load_test_model(self.trainer.checkpoint_callback.last_model_path)
+        lowres, pred, gt = self.get_lowres_pred_gt(fc, mag_min=mag_min,
+                                                   mag_max=mag_max)
+        for i in range(min(3, len(lowres))):
+            lowres_ = torch.clamp((lowres[i].unsqueeze(0) - lowres.min()) / (lowres.max() - lowres.min()), 0, 1)
+            pred_ = torch.clamp((pred[i].unsqueeze(0) - pred.min()) / (pred.max() - pred.min()), 0, 1)
+            gt_ = torch.clamp((gt[i].unsqueeze(0) - gt.min()) / (gt.max() - gt.min()), 0, 1)
 
-        dft_pred = convert_to_dft(fc=pred, mag_min=mag_min, mag_max=mag_max,
-                                  dst_flatten_coords=self.dst_flatten_coords, img_shape=self.hparams.img_shape)
-        dft_target = convert_to_dft(fc=y_fc, mag_min=mag_min, mag_max=mag_max,
-                                    dst_flatten_coords=self.dst_flatten_coords, img_shape=self.hparams.img_shape)
-
-        for i in range(3):
-            pred_img = torch.fft.irfftn(dft_pred[i], s=2 * (self.hparams.img_shape,))
-            y_img = torch.fft.irfftn(dft_target[i], s=2 * (self.hparams.img_shape,))
-
-            pred_img = torch.clamp((pred_img - pred_img.min()) / (pred_img.max() - pred_img.min()), 0, 1)
-            y_img = torch.clamp((y_img - y_img.min()) / (y_img.max() - y_img.min()), 0, 1)
-
-            self.trainer.logger.experiment.add_image('predcitions/img_{}'.format(i), pred_img.unsqueeze(0),
+            self.trainer.logger.experiment.add_image('inputs/img_{}'.format(i), lowres_,
                                                      global_step=self.trainer.global_step)
-            self.trainer.logger.experiment.add_image('groundtruth/img_{}'.format(i), y_img.unsqueeze(0),
+            self.trainer.logger.experiment.add_image('predcitions/img_{}'.format(i), pred_,
+                                                     global_step=self.trainer.global_step)
+            self.trainer.logger.experiment.add_image('targets/img_{}'.format(i), gt_,
                                                      global_step=self.trainer.global_step)
 
     def validation_epoch_end(self, outputs):
@@ -147,31 +136,82 @@ class SResTransformerModule(LightningModule):
         self.log('Train/avg_val_amp_loss', torch.mean(torch.stack(amp_loss)), logger=True, on_epoch=True)
         self.log('Train/avg_val_phi_loss', torch.mean(torch.stack(phi_loss)), logger=True, on_epoch=True)
 
+    def load_test_model(self, path):
+        self.sres_pred = SResTransformerPredict(self.hparams.d_model,
+                                                y_coords_img=self.y_coords_img, x_coords_img=self.x_coords_img,
+                                                flatten_order=self.dst_flatten_order,
+                                                attention_type='causal-linear',
+                                                n_layers=self.hparams.n_layers,
+                                                n_heads=self.hparams.n_heads,
+                                                d_query=self.hparams.d_query,
+                                                dropout=self.hparams.dropout,
+                                                attention_dropout=self.hparams.attention_dropout)
+        if len(path) > 0:
+            weights = torch.load(path)
+            sd = {}
+            for k in weights['state_dict'].keys():
+                sd[k[5:]] = weights['state_dict'][k]
+            self.sres_pred.load_state_dict(sd)
+
+        self.sres_pred.to(self.device)
+
+    def predict_with_recurrent(self, fcs, n, seq_len):
+        memory = None
+        y_hat = []
+        x_hat = []
+
+        with torch.no_grad():
+            for i in range(n):
+                x_hat.append(fcs[:, i])
+                yi, memory = self.sres_pred(x_hat[-1], i=i, memory=memory)
+                y_hat.append(yi)
+
+            for i in range(n, seq_len - 1):
+                x_hat.append(y_hat[-1])
+                yi, memory = self.sres_pred(x_hat[-1], i=i, memory=memory)
+                y_hat.append(yi)
+
+            x_hat.append(y_hat[-1])
+            x_hat = torch.stack(x_hat, dim=1)
+
+        return x_hat
+
+    def convert2img(self, fc, mag_min, mag_max):
+        dft = convert_to_dft(fc=fc, mag_min=mag_min, mag_max=mag_max, dst_flatten_coords=self.dst_flatten_order,
+                             img_shape=self.hparams.img_shape)
+        return torch.fft.irfftn(dft, s=2 * (self.hparams.img_shape,), dim=[1, 2])
+
     def test_step(self, batch, batch_idx):
         fc, (mag_min, mag_max) = batch
-        assert len(fc) == 1, 'Test images have to be evaluated independently.'
-        x_fc = fc[:, self.dst_flatten_coords][:, :-1]
-        y_fc = fc[:, self.dst_flatten_coords][:, 1:]
+        lowres_img, pred_img, gt_img = self.get_lowres_pred_gt(fc=fc, mag_min=mag_min, mag_max=mag_max)
 
-        pred = self.sres.forward(x_fc)
-
-        pred = torch.cat([x_fc[:, :1], pred], dim=1)
-        pred_dft = convert_to_dft(fc=pred, mag_min=mag_min, mag_max=mag_max, dst_flatten_coords=self.dst_flatten_coords,
-                                  img_shape=self.hparams.img_shape)
-        pred_img = torch.fft.irfftn(pred_dft[0], s=2 * (self.hparams.img_shape,))
-
-        y = torch.cat([x_fc[:, :1], y_fc], dim=1)
-        y = convert_to_dft(fc=y, mag_min=mag_min, mag_max=mag_max, dst_flatten_coords=self.dst_flatten_coords,
-                           img_shape=self.hparams.img_shape)
-        y_img = torch.fft.irfftn(y[0], s=2 * (self.hparams.img_shape,))
-
-        gt = denormalize(y_img, self.trainer.datamodule.mean, self.trainer.datamodule.std)
+        lowres_img = denormalize(lowres_img, self.trainer.datamodule.mean, self.trainer.datamodule.std)
         pred_img = denormalize(pred_img, self.trainer.datamodule.mean, self.trainer.datamodule.std)
+        gt_img = denormalize(gt_img, self.trainer.datamodule.mean, self.trainer.datamodule.std)
 
-        return PSNR(gt, pred_img, drange=torch.tensor(255., dtype=torch.float32))
+        lowres_psnr = [PSNR(gt_img[i], lowres_img[i], drange=torch.tensor(255., dtype=torch.float32)) for i in
+                       range(gt_img.shape[0])]
+        pred_psnr = [PSNR(gt_img[i], pred_img[i], drange=torch.tensor(255., dtype=torch.float32)) for i in
+                     range(gt_img.shape[0])]
+        return (lowres_psnr, pred_psnr)
+
+    def get_lowres_pred_gt(self, fc, mag_min, mag_max):
+        x_fc = fc[:, self.dst_flatten_order][:, :self.input_seq_length]
+        pred = self.predict_with_recurrent(x_fc, self.input_seq_length, fc.shape[1])
+        pred_img = self.convert2img(fc=pred, mag_min=mag_min, mag_max=mag_max)
+        lowres = torch.zeros_like(pred)
+        lowres += fc.min()
+        lowres[:, :self.input_seq_length] = fc[:, self.dst_flatten_order][:, :self.input_seq_length]
+        lowres_img = self.convert2img(fc=lowres, mag_min=mag_min, mag_max=mag_max)
+        gt_img = self.convert2img(fc=fc[:, self.dst_flatten_order], mag_min=mag_min, mag_max=mag_max)
+        return lowres_img, pred_img, gt_img
 
     def test_epoch_end(self, outputs):
-        outputs = torch.stack(outputs)
-        self.log('Mean PSNR', torch.mean(outputs).detach().cpu().numpy(), logger=True)
-        self.log('SEM PSNR', torch.std(outputs / np.sqrt(len(outputs))).detach().cpu().numpy(),
+        lowres_psnrs = torch.cat([torch.stack(o[0]) for o in outputs])
+        pred_psnrs = torch.cat([torch.stack(o[1]) for o in outputs])
+        self.log('Input Mean PSNR', torch.mean(lowres_psnrs).detach().cpu().numpy(), logger=True)
+        self.log('Input SEM PSNR', torch.std(lowres_psnrs / np.sqrt(len(lowres_psnrs))).detach().cpu().numpy(),
+                 logger=True)
+        self.log('Prediction Mean PSNR', torch.mean(pred_psnrs).detach().cpu().numpy(), logger=True)
+        self.log('Prediction SEM PSNR', torch.std(pred_psnrs / np.sqrt(len(pred_psnrs))).detach().cpu().numpy(),
                  logger=True)
