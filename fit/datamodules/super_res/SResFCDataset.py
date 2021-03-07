Fix phase normalization, now prediction is in [-1, 1].

fit/datamodules/super_res/SResFCDataset.py
@@ -35,7 +35,7 @@ class SResFourierCoefficientDataset(Dataset):
 
         img_mag = 2 * (img_mag - self.mag_min) / (self.mag_max - self.mag_min) - 1
 
-        img_phi = 2 * img_phi / (2 * np.pi) - 1
+        img_phi = img_phi / np.pi
 
         img_fft = torch.stack([img_mag.flatten(), img_phi.flatten()], dim=-1)
         return img_fft, (self.mag_min.unsqueeze(-1), self.mag_max.unsqueeze(-1))

fit/datamodules/tomo_rec/TRecFCDataset.py
@@ -43,8 +43,8 @@ class TRecFourierCoefficientDataset(Dataset):
         sino_mag = 2 * (sino_mag - self.mag_min) / (self.mag_max - self.mag_min) - 1
         img_mag = 2 * (img_mag - self.mag_min) / (self.mag_max - self.mag_min) - 1
 
-        sino_phi = 2 * sino_phi / (2 * np.pi) - 1
-        img_phi = 2 * img_phi / (2 * np.pi) - 1
+        sino_phi = sino_phi / np.pi
+        img_phi = img_phi / np.pi
 
         sino_fft = torch.stack([sino_mag.flatten(), sino_phi.flatten()], dim=-1)
         img_fft = torch.stack([img_mag.flatten(), img_phi.flatten()], dim=-1)

fit/utils/utils.py
@@ -10,9 +10,7 @@ def convert2FC(x, mag_min, mag_max):
 
 
 def denormalize_phi(phi):
-    phi = (phi + 1) / 2.
-    phi = phi * 2 * np.pi
-    return phi
+    return phi * np.pi
 
 
 def denormalize_amp(amp, mag_min, mag_max):