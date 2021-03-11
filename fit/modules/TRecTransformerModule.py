879c0974d074a9a71a0124c9d352c169fe428366
tibuch <tibuch@mpi-cbg.de>
2021-03-11 19:50:58 +0100

    add TRec-Encoder only option.

--- fit/modules/TRecTransformerModule.py
+++ fit/modules/TRecTransformerModule.py
@@ -4,7 +4,7 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from fit.datamodules.tomo_rec import MNISTTomoFourierTargetDataModule
 from fit.modules.loss import _fc_prod_loss, _fc_sum_loss
-from fit.transformers.TRecTransformer import TRecTransformer
+from fit.transformers.TRecTransformer import TRecTransformer, TRecEncoder
 from fit.utils import convert2FC, fft_interpolate, PSNR, convert_to_dft, psfft
 from fit.utils.RAdam import RAdam
 
@@ -25,7 +25,8 @@ class TRecTransformerModule(LightningModule):
                  bin_factor_cd=10,
                  lr=0.0001,
                  weight_decay=0.01,
-                 attention_type="linear", n_layers=4, n_heads=4, d_query=4, dropout=0.1, attention_dropout=0.1):
+                 attention_type="linear", n_layers=4, n_heads=4, d_query=4, dropout=0.1, attention_dropout=0.1,
+                 encoder_only=False):
         super().__init__()
 
         self.save_hyperparameters("d_model",
@@ -75,7 +76,8 @@ class TRecTransformerModule(LightningModule):
         else:
             self.random_cond = None
 
-        self.trec = TRecTransformer(d_model=self.hparams.d_model,
+        if encoder_only:
+            self.trec = TRecEncoder(d_model=self.hparams.d_model,
                                     y_coords_proj=y_coords_proj, x_coords_proj=x_coords_proj,
                                     flatten_proj=self.src_flatten_coords,
                                     y_coords_img=y_coords_img, x_coords_img=x_coords_img,
@@ -86,6 +88,18 @@ class TRecTransformerModule(LightningModule):
                                     d_query=self.hparams.d_query,
                                     dropout=self.hparams.dropout,
                                     attention_dropout=self.hparams.attention_dropout)
+        else:
+            self.trec = TRecTransformer(d_model=self.hparams.d_model,
+                                        y_coords_proj=y_coords_proj, x_coords_proj=x_coords_proj,
+                                        flatten_proj=self.src_flatten_coords,
+                                        y_coords_img=y_coords_img, x_coords_img=x_coords_img,
+                                        flatten_img=self.dst_flatten_coords,
+                                        attention_type=self.hparams.attention_type,
+                                        n_layers=self.hparams.n_layers,
+                                        n_heads=self.hparams.n_heads,
+                                        d_query=self.hparams.d_query,
+                                        dropout=self.hparams.dropout,
+                                        attention_dropout=self.hparams.attention_dropout)
 
         x, y = torch.meshgrid(torch.arange(-self.hparams.img_shape // 2 + 1,
                                            self.hparams.img_shape // 2 + 1),
--- fit/transformers/TRecTransformer.py
+++ fit/transformers/TRecTransformer.py
@@ -97,3 +97,71 @@ class TRecTransformer(torch.nn.Module):
         img_post += img_hat
 
         return y_hat, img_post[:, 0]
+
+
+class TRecEncoder(torch.nn.Module):
+    def __init__(self,
+                 d_model,
+                 y_coords_proj, x_coords_proj, flatten_proj,
+                 y_coords_img, x_coords_img, flatten_img,
+                 attention_type="linear",
+                 n_layers=4,
+                 n_heads=4,
+                 d_query=32,
+                 dropout=0.1,
+                 attention_dropout=0.1):
+        super(TRecEncoder, self).__init__()
+
+        self.fbp_fourier_coefficient_embedding = torch.nn.Linear(2, d_model // 2)
+
+        self.pos_embedding_target = PositionalEncoding2D(d_model // 2, y_coords_img, x_coords_img,
+                                                         flatten_order=flatten_img)
+
+        self.encoder = TransformerEncoderBuilder.from_kwargs(
+            attention_type=attention_type,
+            n_layers=n_layers,
+            n_heads=n_heads,
+            feed_forward_dimensions=n_heads * d_query * 4,
+            query_dimensions=d_query,
+            value_dimensions=d_query,
+            dropout=dropout,
+            attention_dropout=attention_dropout
+        ).get()
+
+        self.predictor_amp = torch.nn.Linear(
+            n_heads * d_query,
+            1
+        )
+        self.predictor_phase = torch.nn.Linear(
+            n_heads * d_query,
+            1
+        )
+
+        self.conv_block = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0)
+        )
+
+    def forward(self, x, fbp, mag_min, mag_max, dst_flatten_coords, img_shape, attenuation):
+        fbp = self.fbp_fourier_coefficient_embedding(fbp)
+        fbp = self.pos_embedding_target(fbp)
+        y_hat = self.encoder(fbp)
+        y_amp = self.predictor_amp(y_hat)
+        y_phase = F.tanh(self.predictor_phase(y_hat))
+        y_hat = torch.cat([y_amp, y_phase], dim=-1)
+
+        dft_hat = convert_to_dft(y_hat, mag_min=mag_min, mag_max=mag_max, dst_flatten_coords=dst_flatten_coords,
+                                 img_shape=img_shape)
+        dft_hat *= attenuation
+        img_hat = torch.roll(torch.fft.irfftn(dft_hat, dim=[1, 2], s=2 * (img_shape,)),
+                             2 * (img_shape // 2,), (1, 2)).unsqueeze(1)
+        img_post = self.conv_block(img_hat)
+        img_post += img_hat
+
+        return y_hat, img_post[:, 0]
+
